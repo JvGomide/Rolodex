24127da32f9d47dcc87bfc1234887af7f8e15fb5
Jvgomide <joaovitor_m4ster@hotmail.com>
2023-02-16 18:01:29 -0300

    Aula 35 completa

--- src/App.js
+++ src/App.js
@@ -8,19 +8,37 @@ class App extends Component{
     super();
     
     this.state = {
-      name: {firstName: 'João', lastName: 'Gomide'},
-      company: 'Acelera XYZ'
+      monsters: [
+        {
+          name: 'Linda',
+          id: '1abc'
+        },
+        {
+          name: 'Frank',
+          id: '1bcd'
+        },
+        {
+          name: 'Jack',
+          id: '1cde'
+        },
+        {
+          name: 'Andrei',
+          id: '1def'
+        }
+      ]
     };
   }
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work in {this.state.company}</p>
-          <button onClick={() => { this.setState({name: {firstName: 'Jv', lastName: 'Tralha'}})}}>Change name!</button>
-        </header>
+        { this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
