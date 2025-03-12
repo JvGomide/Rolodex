920e7a3523be45bc7a7698629e33b4cdfcb9feac
joao-gomide <joao.vgc@puccampinas.edu.br>
2025-03-12 12:35:38 -0300

    [feat]: Changed class components to function components, all working, ep64 done!

--- src/App.js
+++ src/App.js
@@ -1,74 +1,44 @@
-import { Component } from 'react';
-
-//import logo from './logo.svg';
-import './App.css';
-import './components/cardList/card-list.component';
+import { useState, useEffect } from 'react';
 import CardList from './components/cardList/card-list.component';
 import SearchBox from './components/searchBox/searchBox.component';
+import './App.css';
 
-class App extends Component{
-  constructor(){
-    super();
-    
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-  }
+//Temos dois tipos de funções, puras e impuras.
+//Funções puras dependem completamente dos argumentos passados a ela.
+//Funções impuras tem elementos externos aos parametros que alteram o resultado da função, mesmo que os parametros não tenham mudado. Funções impuras também pode ser categorizado
+//pelos seus side effects ao alterar algo fora do escopo da função
+const App = () => {
 
-  //Primeira vez que um componente é carregado no DOM, só acontece uma vez no ciclo de vida de um componente
-  componentDidMount(){
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters]  = useState(monsters);
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) =>  response.json())
-      //Todo o valor obtido pelo response é passado para o próximo .then como users
-      .then((users) => this.setState(() => {
-        return {monsters: users};
-      },
+      .then((users) => setMonsters(users));
+  }, []);
 
-      () => {}
-
-      ));
-  }
-
-  // Para otimizar a perfomance do código, uma função que pode ser utilizada muitas vezes não precisa ser anônima, crie uma função para ela.
-  //Antes quando a função estava dentor do onChange, toda vez que o render era re-iniciado, a função era re-criada, usada e depois era jogada fora
-  //Agora que ela está atrelada a classe, ela é criada apenas uma vez.
-  onSearchChange = (event) => {
-
-    const searchField = event.target.value.toLowerCase();
-
-    //makes the page render again
-    this.setState(() => {
-      return { searchField };
-    })
-  }
-
-
-  // É atualizado toda vez que o state é atualizado/sofre mudanças
-  render(){
-
-    //Para evitar repetição do 'this' em todo lugar podemos fazer uso da des-estrurização (destructuring)
-    //Ajuda na leitura
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    //Método Filter cria um novo array a partir de um já pré-existente, no qual o novo array gerado, retornará tudo aquilo que for TRUE dentro do filtro que você escolheu
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
-      //includes(): se algo está incluso em outro algo, retorna booleano (true, false)
-      //includes(): não diferencia cases
-      //React best practices: Imutabilidade, evite modificar um array, sempre que possivel, gere um novo com as modificações feitas
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
-    return (
-      <div className="App">
-        <h1 className='app-title'>Rolexdex</h1>
-        <SearchBox onChangeHandler={onSearchChange} placeholder='Type to find a Monster' className='monster-search-box'/>
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+      const searchFieldsString = event.target.value.toLocaleLowerCase();
+      setSearchField(searchFieldsString);
   }
-  
-}
+
+  return(
+    <div className="App">
+      <h1 className='app-title'>Rolexdex</h1>
+      <SearchBox onChangeHandler={onSearchChange} placeholder='Type to find a Monster' className='monster-search-box'/>
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
